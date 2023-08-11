f84042bb7dc11f42fc4df4c7a5db81f93f326f88
Ray S <61741634+suemnick@users.noreply.github.com>
2023-08-11 13:08:45 +0200

    validation for quiz config implemented

--- src/app/quiz-config/quiz-config.component.ts
+++ src/app/quiz-config/quiz-config.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz-config',
@@ -10,9 +10,24 @@ export class QuizConfigComponent {
   quizConfigForm = new FormGroup({
     categorySelect: new FormControl('placeholder'),
     difficultySelect: new FormControl('placeholder')
-  });
+  }, { validators: quizConfigValidator });
 
   createQuiz() {
-
+    console.log("createQuiz called");
   }
 }
+
+export const quizConfigValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const categorySelect = control.get('categorySelect');
+  const difficultySelect = control.get('difficultySelect');
+  console.log(`validating ${categorySelect} and ${difficultySelect}...`);
+  if (categorySelect && categorySelect.value === 'placeholder') {
+    return { quizConfig: 'category' };
+  }
+
+  if (difficultySelect && difficultySelect.value === 'placeholder') {
+    return { quizConfig: 'difficulty' };
+  }
+
+  return null;
+};
